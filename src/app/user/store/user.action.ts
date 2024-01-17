d761df765b7c105e53cbc1bb927e693881f8049d
Doroshkeviches <Doroshkeviches@yandex.ru>
2024-01-17 18:30:49 +0300

    add devices

--- src/app/user/store/user.action.ts
+++ src/app/user/store/user.action.ts
@@ -3,12 +3,25 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 // ======= utils, types ======= //
 import repository from 'src/repository';
 import { User } from '../../user/types/User.type';
+import { Device } from '../types/Device.type';
 
 export const getUser = createAsyncThunk<User, string>(
   'Get/user',
   async (id, { rejectWithValue }) => {
     try {
       const response = await repository.get(`/user/current/${id}`);
+      return response.data;
+    } catch (error: any) {
+      return rejectWithValue(error);
+    }
+  }
+);
+
+export const getUserDevices = createAsyncThunk<Device[]>(
+  'Get/userDevices',
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await repository.get(`/devices`);
       return response.data;
     } catch (error: any) {
       return rejectWithValue(error);
