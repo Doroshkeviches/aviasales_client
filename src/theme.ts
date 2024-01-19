2bd491425a9e0effe651f2df9bc774048d3e5bf3
sevichok <90391121+sevichok@users.noreply.github.com>
2024-01-19 20:07:43 +0300

    fix client header

--- src/theme.ts
+++ src/theme.ts
@@ -212,6 +212,22 @@ export const theme = createTheme({
       fontSize: FontSize.H4,
       fontWeight: FontWeight.Normal,
       lineHeight: '1.428571',
+      '@media (max-width: 785px)': {
+        lineHeight: '1.5',
+        fontSize: FontSize.H5,
+      },
+      '@media (max-width: 520px)': {
+        lineHeight: '1.3',
+        fontSize: FontSize.H6,
+      },
+      '@media (max-width: 460px)': {
+        lineHeight: '1.2',
+        fontSize: FontSize.H6,
+      },
+      '@media (max-width: 360px)': {
+        lineHeight: '1',
+        fontSize: FontSize.H6,
+      },
       '&.main': {
         color: colors.whitesmoke,
       },
@@ -726,6 +742,15 @@ export const theme = createTheme({
             color: colors.whitesmoke,
             backgroundColor: colors.errorBlend,
 
+            '@media (max-width: 785px)': {
+              lineHeight: '1.2',
+              fontSize: FontSize.H5,
+              padding: '2px 6px',
+            },
+            '@media (max-width: 520px)': {
+              fontSize: FontSize.H6,
+              padding: '2px 5px',
+            },
             '&:hover': {
               backgroundColor: colors.error,
             },
