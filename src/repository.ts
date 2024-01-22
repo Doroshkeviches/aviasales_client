d0f4c9a677fb47a12ff6f1dc7c7e21606c0eb24c
sevichok <90391121+sevichok@users.noreply.github.com>
2024-01-22 20:03:43 +0300

    env file usage

--- src/repository.ts
+++ src/repository.ts
@@ -1,9 +1,9 @@
-import axios, { AxiosError, AxiosRequestHeaders } from "axios";
-import { baseUrl, client_host } from "./constants";
-import { RoutesConstant } from "./constants/RoutesConstants.enum";
-const { REACT_APP_API_URL } = process.env;
+import axios, { AxiosError, AxiosRequestHeaders } from 'axios';
+import { RoutesConstant } from './constants/RoutesConstants.enum';
+const { REACT_APP_BASE_URL, REACT_APP_CLIENT_HOST } = process.env;
+
 const repository = axios.create({
-  baseURL: REACT_APP_API_URL,
+  baseURL: REACT_APP_BASE_URL,
 });
 
 repository.interceptors.request.use(
@@ -21,43 +21,41 @@ repository.interceptors.request.use(
     return Promise.reject(error);
   }
 );
-repository.interceptors.response.use((response) => {
+repository.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      const refresh_token = localStorage.getItem('refresh-token');
+      try {
+        const tokens = await axios.get(REACT_APP_BASE_URL + '/auth/refresh', {
+          headers: {
+            Authorization: `Bearer ${refresh_token}`,
+          },
+        });
 
-  return response
-}, async (error) => {
-  const originalRequest = error.config;
-  if (error.response?.status === 401 && !originalRequest._retry) {
-    originalRequest._retry = true;
-    const refresh_token = localStorage.getItem('refresh-token')
-    try {
-      const tokens = await axios.get(baseUrl + '/auth/refresh', {
-        headers: {
-          Authorization: `Bearer ${refresh_token}`,
+        if (!tokens) {
+          localStorage.removeItem('refresh-token');
+          return;
         }
-      });
+        sessionStorage.setItem('access-token', tokens.data.access_token);
+        localStorage.setItem('refresh-token', tokens.data.refresh_token);
 
-      if (!tokens) {
-        localStorage.removeItem('refresh-token')
-        return
+        // Retry the original request with the new token
+        return repository(originalRequest);
+      } catch (refreshError) {
+        localStorage.removeItem('refresh-token');
+        sessionStorage.removeItem('access-token');
+        window.location.replace(REACT_APP_CLIENT_HOST + RoutesConstant.sign_in);
+        return Promise.reject(refreshError);
       }
-      sessionStorage.setItem('access-token', tokens.data.access_token);
-      localStorage.setItem('refresh-token', tokens.data.refresh_token)
-
-      // Retry the original request with the new token
-      return repository(originalRequest);
-    } catch (refreshError) {
-      localStorage.removeItem('refresh-token')
-      sessionStorage.removeItem('access-token')
-      window.location.replace(client_host + RoutesConstant.sign_in)
-      return Promise.reject(refreshError);
+    } else {
+      return Promise.reject(error);
     }
-  } else {
-    return Promise.reject(error);
   }
-
-});
-
-
-
+);
 
 export default repository;
