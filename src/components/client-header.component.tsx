e0c661dba21d16c0ca5fd681c12c8656531ad2d5
sevichok <90391121+sevichok@users.noreply.github.com>
2024-01-23 16:21:59 +0300

    add chat route

--- src/components/client-header.component.tsx
+++ src/components/client-header.component.tsx
@@ -14,13 +14,14 @@ const ClientHeaderComp: FC = () => {
     const navToFlights = () => navigate(RoutesConstant.flights)
     const navToCart = () => navigate(RoutesConstant.cart)
     const navToProfile = () => navigate(RoutesConstant.user)
+    const navToChat= () => navigate(RoutesConstant.chat)
 
     return (
         <header style={{ display: 'flex', padding: '10px 60px', justifyContent: 'center' }}>
             <MenuItem onClick={navToFlights}>
                 <Typography variant="h4" className="navlink">Flights</Typography>
             </MenuItem>
-            <MenuItem>
+            <MenuItem onClick={navToChat}>
                 <Typography variant="h4" className="navlink">Chat</Typography>
             </MenuItem>
             <MenuItem onClick={navToProfile}>
